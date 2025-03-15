6a119feaab16664010544154da6a8a1ad9c13627
fahri-r <00.fahri.r@gmail.com>
2025-03-15 20:39:03 +0700

    fix: Migrate from io.springfox to org.springdoc

--- src/main/java/com/astrapay/controller/ExampleController.java
+++ src/main/java/com/astrapay/controller/ExampleController.java
@@ -3,10 +3,6 @@ package com.astrapay.controller;
 import com.astrapay.dto.ExampleDto;
 import com.astrapay.exception.ExampleException;
 import com.astrapay.service.ExampleService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Api(value = "ExampleController")
 @Slf4j
 public class ExampleController {
     private final ExampleService exampleService;
@@ -27,12 +22,6 @@ public class ExampleController {
     }
 
     @GetMapping("/hello")
-    @ApiOperation(value = "Say Hello")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(code = 200, message = "OK", response = ExampleDto.class)
-            }
-    )
     public ResponseEntity<String> sayHello(@RequestParam String name, @RequestParam String description) {
         log.info("Incoming hello Request from " + name);
 
